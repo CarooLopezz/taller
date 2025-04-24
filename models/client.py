34aaf522130b9e0875f6499aa4466e7b031c7897
Carolina Lopez <caroolinalopez4@gmail.com>
2025-04-23 22:33:53 -0300

    clase cliente

--- models/client.py
+++ models/client.py
@@ -7,21 +7,19 @@ from models.db import db #type: ignore
 
 class Client(db.model):
 
-    __tablename__='client' #creo la tabla cliente
+    """ __tablename__='client' #creo la tabla cliente
     id=db.Column(db.Integer, primary_key=True)
     name=db.Column(db.String, nullable=True)
     email=db.Column(db.String, nullable=True, unique=True)
-    phone=db.Column(db.String, nullnable=True)
+    phone=db.Column(db.String, nullnable=True) """
+    
+    class Cliente:
+        def __init__(self, nombre, direccion, celular, email):
+            self.nombre = nombre
+            self.direccion = direccion
+            self.celular = celular
+            self.email = email
+            
+        def mostrar_informacion(self):
+            return f"Cliente: {self.nombre}, Dirección: {self.direccion}, Celular: {self.celular}, Email: {self.email}"
 
-    def __init__(self,name,email,phone):
-        self.name=name
-        self.email=email
-        self.phone=phone
-
-    def serialize(self):
-        return{
-            'id':self.id,
-            'name':self.name,
-            'email':self.email,
-            'phone':self.phone,
-        }
