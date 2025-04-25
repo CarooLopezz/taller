1620d959b2fa019147991b00c0129fe43584cee6
Aeantunez <aeantunez@hotmail.com.argit config --global user.email aeantunez@hotmail.com.ar>
2025-04-25 00:11:15 -0300

    Vehiculo

--- routes/product.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-product=Blueprint("product", __name__)
-
-@product.route("/api/products")#traigo todas las rutas
-def getAllProducts():
-    return ("Get all products!") 
-#protocolo de comunicacion put get delete post http
